da8c945e3c4cc132a703344d1e90ba67b253b473
Joelxd12O <joel.game100xd@gmail.com>
2026-02-07 10:21:09 -0500

    refactor: mejorar la configuración de NixOS y optimizar la gestión de energía

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,251 +1,299 @@
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+# Configuración de NixOS
+# https://search.nixos.org/options
 
 { config, lib, pkgs, ... }:
-#zen
+
 let
   zen-browser = import (builtins.fetchTarball "https://github.com/youwen5/zen-browser-flake/archive/master.tar.gz") { inherit pkgs; };
 in
 {
-  imports =
-    [ ./hardware-configuration.nix ];
+  # ============================================
+  # IMPORTS
+  # ============================================
+  imports = [ ./hardware-configuration.nix ];
 
-# Use the systemd-boot EFI boot loader.
-boot.loader = {
-  efi = {
-    canTouchEfiVariables = true;
-    efiSysMountPoint = "/boot";
-  };
-
-  systemd-boot.enable = false;
-
-  grub = {
-    enable = true;
-    device = "nodev";
-    efiSupport = true;
-    useOSProber = true;
-
-    gfxmodeEfi = "1280x720";
-    timeoutStyle = "menu";
+  # ============================================
+  # BOOTLOADER
+  # ============================================
+  boot.loader = {
     timeout = 5;
-    configurationLimit = 3;
 
-    theme = pkgs.fetchFromGitHub {
-      owner = "callmenoodles";
-      repo = "space-isolation";
-      rev = "2f172f7cb6769bbef8dec62738ac168698c48985";
-      sha256 = "sha256-YAzrqaTi9TvoSPsefYAW1ksMBu0yg92jAzFmk7l7shI=";
-    } + "/1920x1080";
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+
+    systemd-boot.enable = false;
+
+    grub = {
+      enable = true;
+      device = "nodev";
+      efiSupport = true;
+      useOSProber = true;
+      gfxmodeEfi = "1280x720";
+      timeoutStyle = "menu";
+      configurationLimit = 3;
+
+      theme = pkgs.fetchFromGitHub {
+        owner = "callmenoodles";
+        repo = "space-isolation";
+        rev = "2f172f7cb6769bbef8dec62738ac168698c48985";
+        sha256 = "sha256-YAzrqaTi9TvoSPsefYAW1ksMBu0yg92jAzFmk7l7shI=";
+      } + "/1920x1080";
+    };
   };
-};
 
-
-
+  # ============================================
+  # NETWORKING & LOCALIZATION
+  # ============================================
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
   time.timeZone = "America/Lima";
   i18n.defaultLocale = "es_PE.UTF-8";
-  
+  services.xserver.xkb.layout = "latam";
 
-  services.xserver.xkb.layout = "latam"; #us
+  # ============================================
+  # LOGIN & SESSION
+  # ============================================
+  # Login automático para el usuario joel
+  services.getty.autologinUser = "joel";
 
-# Habilitar el login automático para el usuario falo
-services.getty.autologinUser = "joel";
-
-#y ejecutar hyprland de una
+  # Ejecutar Hyprland automáticamente en tty1
   environment.loginShellInit = ''
     if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
       exec hyprland
     fi
   '';
-# presunto fix para lo de hyprland que se cierran las apps apenas las abro
+
+  # Variables de entorno para Wayland/Hyprland
   environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1"; # Fuerza a apps de Electron (como ZapZap) a usar Wayland
+    NIXOS_OZONE_WL = "1";
     XDG_CURRENT_DESKTOP = "Hyprland";
     XDG_SESSION_TYPE = "wayland";
     XDG_SESSION_DESKTOP = "Hyprland";
-    # Fix para temas oscuros y Dolphin
     GTK_THEME = "Adwaita:dark";
     QT_QPA_PLATFORMTHEME = "qt6ct";
     QT_QPA_PLATFORM = "wayland";
   };
 
-
+  # ============================================
+  # USUARIOS
+  # ============================================
   users.mutableUsers = true;
   users.users.joel = {
-  isNormalUser = true;
-  extraGroups = [
-    "wheel"
-    "networkmanager"
-    "audio"
-    "video"
-    "input"
-    "docker"
-  ];
-};
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "audio"
+      "video"
+      "input"
+      "docker"
+    ];
+  };
 
-hardware.pulseaudio.enable = false;
-security.rtkit.enable = true;
+  # ============================================
+  # HARDWARE & SERVICIOS BASE
+  # ============================================
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
 
-
+  # Programas del sistema
   programs.firefox.enable = true;
   programs.hyprland.enable = true;
+  programs.hyprlock.enable = true;
   programs.git.enable = true;
+  programs.nix-ld.enable = true;
 
-#Lock obligatorio en Hyprland
+  # Gestión de energía y suspensión automática
+  services.logind = {
+    enable = true;
+    idleAction = "suspend";
+    idleActionSec = "30m";              # Suspender tras 30 minutos de inactividad
+    lidSwitchExternalPower = "suspend";
+    handlePowerKey = "suspend";
+    powerKeyLongPress = "poweroff";
+  };
 
-programs.hyprlock.enable = true;
-
-# Suspensión automática por inactividad (como Windows)
-services.logind = {
-  enable = true;
-  idleAction = "suspend";              # Acción cuando se alcanza inactividad: suspender
-  idleActionSec = "10s";               # 10 segundos de prueba
-  lidSwitchExternalPower = "suspend";  # Cierre de tapa = suspender
-  handlePowerKey = "suspend";          # Botón power = suspender
-  powerKeyLongPress = "poweroff";      # Power largo = apagar
-};
-
-  # You can use https://search.nixos.org/ to find more packages (and options).
+  # ============================================
+  # PAQUETES
+  # ============================================
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
-# entorno
-    git
-    github-cli
-    neovim
-    kitty
-    hyprlock
-    hyprpaper
-    hypridle
-    xidletime
-    waybar
-    rofi
-    zapzap
-    dunst
-    fastfetch
-    btop
-    nix-prefetch-github
-    #cositas jejeje
-    cava
-    cmatrix
-    tty-clock
-  swaylock-effects
-
+    # Sistema base
     vim
     wget
     curl
-    terminus_font
-    easyeffects
-    os-prober
     unzip
-    efibootmgr
-    zen-browser.default
-    wev
+
+    # Git y desarrollo
+    git
+    github-cli
+
+    # Terminal y CLI
+    neovim
+    kitty
     starship
     atuin
     blesh
+    fastfetch
+    btop
+    ranger
 
-#audio y brillo
+    # Entorno Hyprland
+    hyprlock
+    hyprpaper
+    hypridle
+    waybar
+    rofi
+    dunst
+    wev
+
+    # Audio y multimedia
     pavucontrol
     playerctl
+    pamixer
+    easyeffects
+    cava
+
+    # Bluetooth
     blueman
     bluez
-    pamixer
-    brightnessctl
-    glib
-    
-#Dolphin
-    ranger # FCK DOLPHIN RANGER LO ES TODO (recien lo voy a probar)
 
-    kdePackages.dolphin
-    kdePackages.qtwayland        # Soporte nativo para Wayland
-    kdePackages.qtsvg            # Para que los iconos se vean bien
-    kdePackages.kio-extras       # Miniaturas y funciones extra
-    adwaita-qt                   # Tema para que apps Qt parezcan de GNOME/Modernas (opcional)
-    libsForQt5.qtstyleplugin-kvantum # Si quieres temas muy personalizados
-    libsForQt5.qt5ct
-    kdePackages.qt6ct
-    adwaita-qt6
-#imagenes y capturas
+    # Brillo
+    brightnessctl
+
+    # Capturas de pantalla
     grim
     slurp
-    imv
     wl-clipboard
+
+    # Visualización de imágenes
+    imv
     gimp
-# lenguajes
+
+    # File managers
+    kdePackages.dolphin
+    kdePackages.qtwayland
+    kdePackages.qtsvg
+    kdePackages.kio-extras
+
+    # Temas Qt
+    adwaita-qt
+    adwaita-qt6
+    libsForQt5.qt5ct
+    kdePackages.qt6ct
+    libsForQt5.qtstyleplugin-kvantum
+
+    # Cursores
+    bibata-cursors
+
+    # Fuentes
+    terminus_font
+    glib
+
+    # Lenguajes de programación
     gcc
     gnumake
     python3
     pyright
     nodejs
+
+    # Language servers
     nodePackages.intelephense
-    typescript-language-server # (ts_ls)
-    vscode-langservers-extracted # (eslint)
-    ripgrep     #para nvim telescope
-    fd          #para nvim telescope
-#apps
-    zoom-us
-    steam
+    typescript-language-server
+    vscode-langservers-extracted
+
+    # Herramientas para Neovim
+    ripgrep
+    fd
+
+    # Navegadores
+    zen-browser.default
     google-chrome
-    bibata-cursors
+
+    # Aplicaciones
+    zapzap
     discord
     spotify
     spicetify-cli
     obsidian
     vscode
-    #cyberseguridad
+    zoom-us
+
+    # Juegos
+    steam
+
+    # Diversión
+    cmatrix
+    tty-clock
+
+    # Seguridad
     wireshark
+
+    # Utilidades NixOS
+    nix-prefetch-github
+    os-prober
+    efibootmgr
   ];
 
-#audio
+  # ============================================
+  # AUDIO (PipeWire)
+  # ============================================
   services.pipewire = {
-  enable = true;
-  alsa.enable = true;
-  alsa.support32Bit = true;
-  pulse.enable = true;
-  jack.enable = true;
-};
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
 
-#bluetooth
+  # ============================================
+  # BLUETOOTH
+  # ============================================
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
-#steam
-hardware.graphics = {
-  enable = true;
-  enable32Bit = true; # Muy importante para Steam
-};
 
-programs.steam = {
-  enable = true;
-  remotePlay.openFirewall = true; # Opcional: Abre puertos para Remote Play
-  dedicatedServer.openFirewall = true; # Opcional: Abre puertos para servidores
-};
-# Docker
-virtualisation.docker = {
-  enable = true;
-};
+  # ============================================
+  # GAMING (Steam)
+  # ============================================
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
 
+  # ============================================
+  # VIRTUALIZACIÓN (Docker)
+  # ============================================
+  virtualisation.docker = {
+    enable = true;
+  };
 
-
-#fuentes
+  # ============================================
+  # FUENTES
+  # ============================================
   fonts.fontconfig.enable = true;
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
     nerd-fonts.ubuntu-mono
     font-awesome
-#para instalar la fuente q permite ver los iconos del rofi powermenu debo descargarla del repo https://github.com/adi1090x/rofi/blob/master/fonts/Icomoon-Feather.ttf y pegarla en ~/.local/share/fonts/
+    # Nota: Icomoon-Feather debe instalarse manualmente en ~/.local/share/fonts/
+    # Descargar de: https://github.com/adi1090x/rofi/blob/master/fonts/Icomoon-Feather.ttf
   ];
 
-
-
-#para que nixos pueda ejecutar binarios externos (mason)
-  programs.nix-ld.enable = true;
-
+  # ============================================
+  # OPTIMIZACIONES
+  # ============================================
   nix.settings.auto-optimise-store = true;
+
+  # ============================================
+  # VERSION DEL SISTEMA
+  # ============================================
   system.stateVersion = "25.11";
 }
-
-
-
-
-
